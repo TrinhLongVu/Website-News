679de1b99c700762c2b267e3230660a4f4819eb4
vucongleo@gmail.com <vucongleo@gmail.com>
2023-11-21 15:33:54 +0700

    back-end create MVC model version 1

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('Hello World!');
-    res.end();
-}).listen(3000);
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const tourRouter = require('./routes/articleRouters.js')
+const userRouter = require('./routes/userRouters.js')
+
+// config middleware using req.body
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use('/api/v1/article', tourRouter);
+app.use('/api/v1/user', userRouter);
+
+module.exports = app;
---
+++ back-end/controllers/userController.js
@@ -0,0 +1,29 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).send({
+        status: "error",
+    })
+}
+
+exports.createUser = (req, res) => {
+    res.status(500).send({
+        status: "error",
+    })
+}
+
+exports.getUser = (req, res) => {
+    res.status(500).send({
+        status: "error",
+    })
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).send({
+        status: "error",
+    })
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).send({
+        status: "error",
+    })
+}
