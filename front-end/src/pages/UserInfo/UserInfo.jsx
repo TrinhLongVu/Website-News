f762f6b52d1f93145124e6233bf9d75a2341452f
21127535 <ttnhan21@clc.fitus.edu.vn>
2023-11-28 17:24:37 +0700

    Front-end UserInfo Page - version 2

--- front-end/src/pages/UserInfo/UserInfo.jsx
+++ front-end/src/pages/UserInfo/UserInfo.jsx
@@ -1,19 +1,19 @@
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
-
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-
 import "./user-info.css";
 
 const UserInfo = () => {
   return (
     <>
       <Header />
+
       <div className="avt-account-info">
         <div className="avt">
           <div className="avatar-big"></div>
-          <a className="change-profile-pic" href="#">
+          <a className="change-profile-pic-btn" href="#">
             <FontAwesomeIcon icon={faEdit} /> Change
           </a>
           <a className="sign-out" href="#">
@@ -22,41 +22,99 @@ const UserInfo = () => {
         </div>
 
         <div className="account-info">
-          <h1 id="info-title">Account Information</h1>
+          <h1 className="info-title">Account Information</h1>
 
-          <h3>Username</h3>
+          <div className="info-field">
+            <h3>Username</h3>
+            <input
+              className="info-inp"
+              type="text"
+              name="username"
+              id="username"
+              defaultValue="Simon"
+              readOnly
+            />
+          </div>
+
+          <div className="info-field">
+            <h3>Email</h3>
+            <input
+              className="info-inp"
+              type="email"
+              name="email"
+              id="email"
+              defaultValue="sg@gmail.com"
+              readOnly
+            />
+          </div>
+
+          <div className="info-field">
+            <h3>Bio</h3>
+            <textarea
+              className="bio-inp"
+              name="bio"
+              id="bio"
+              readOnly
+              cols="30"
+              rows="10"
+              defaultValue="Hi everyone, this is the bio of my account"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+
+      <div className="personal-info">
+        <h1 className="info-title">Personal Information</h1>
+
+        <div className="info-field">
+          <h3>Birthday</h3>
           <input
             className="info-inp"
             type="text"
-            name=""
-            id=""
-            defaultValue="Simon"
+            name="birthday"
+            id="birthday"
+            placeholder="No data"
             readOnly
           />
-          <h3>Email</h3>
+        </div>
+
+        <div className="info-field">
+          <h3>Gender</h3>
           <input
             className="info-inp"
-            type="email"
-            name=""
-            id=""
-            defaultValue="sg@gmail.com"
+            type="text"
+            name="gender"
+            id="gender"
+            placeholder="No data"
             readOnly
           />
-          <h3>Bio</h3>
-          <textarea
-            className="bio-inp"
-            name=""
-            id=""
+        </div>
+
+        <div className="info-field">
+          <h3>Phone number</h3>
+          <input
+            className="info-inp"
+            type="text"
+            name="phonenumber"
+            id="phonenumber"
+            placeholder="No data"
             readOnly
-            cols="30"
-            rows="10"
-            defaultValue="Hi everyone, this is the bio of my account"
-          ></textarea>
+          />
+        </div>
+
+        <div className="info-field">
+          <h3>Address</h3>
+          <input
+            className="info-inp"
+            type="text"
+            name="address"
+            id="address"
+            placeholder="No data"
+            readOnly
+          />
         </div>
       </div>
-      <div className="personal-info">
-        <h1>Personal Information</h1>
-      </div>
+
       <Footer />
     </>
   );
