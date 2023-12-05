6be194baf68ba596654616aae0dfe1f89a772838
caokhanh2k3 <127649054+caokhanh2k3@users.noreply.github.com>
2023-12-05 23:56:03 +0700

    v1

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,14 +1,74 @@
-exports.getAllUsers = (req, res) => {
-    res.status(500).send({
-        status: "error",
-    })
+const fs = require('fs');
+const User = require('../models/userModel')
+
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const alldata = await User.find()
+
+        res.status(201).json({
+            status: 'success',
+            data: alldata
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
 }
 
-exports.createUser = (req, res) => {
-    res.status(500).send({
-        status: "error",
-    })
+exports.createUser = async (req, res, next) => {
+
+    try {
+        //===========check username (email) available==========================
+        // Lấy username từ request body
+        const UserName = req.body.username;
+
+        console.log(">>>> da den day", UserName);
+
+        // Kiểm tra xem username đã tồn tại hay chưa
+        const isTaken = await User.findOne({ UserName });
+
+        // Nếu username đã tồn tại, trả về lỗi
+        if (isTaken) {
+            return res.status(400).json({
+                status: "fail",
+                msg: "Username is already taken",
+            });
+        }
+
+        //===========check confirm Password==========================
+        const { password } = req.body;
+        const { confirmPassword } = req.body;
+
+        if (password != confirmPassword) {
+            return res.status(400).json({
+                status: "fail",
+                msg: "incorrect Confirm Password",
+            });
+        }
+
+        const NewBody = {
+            UserName: req.body.username,
+            Password: req.body.password,
+        }
+
+        const newUser = await User.create(NewBody);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                user: newUser
+            }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
 }
+
 
 exports.getUser = (req, res) => {
     res.status(500).send({
@@ -22,8 +82,38 @@ exports.updateUser = (req, res) => {
     })
 }
 
-exports.deleteUser = (req, res) => {
-    res.status(500).send({
-        status: "error",
-    })
+exports.deleteUser = async (req, res, next) => {
+    try {
+
+        const _id = req.params.id;
+
+        console.log(">>>> da den day 1 ", _id);
+        // TO DO:
+        // Find the user by ID and delete it
+        const deletedUser = await User.deleteOne({ _id });
+
+        console.log(">>>> da den day 2", _id);
+
+        if (!deletedUser) {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'User not found.',
+            });
+        }
+
+        // const newUser = req.body
+        const allUsers = await User.find()
+
+
+        res.status(201).json({
+            status: 'success',
+            data: allUsers
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
 }
--- back-end/routes/userRouters.js
+++ back-end/routes/userRouters.js
@@ -1,5 +1,4 @@
 const express = require('express')
-
 const router = express.Router();
 
 const userController = require('../controllers/userController')
