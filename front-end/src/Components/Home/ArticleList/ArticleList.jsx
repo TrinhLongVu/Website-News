87e810bbc95c99003d3b0d21efbb6f0f4ef4c01c
SimonGin <tt26419@gmail.com>
2023-12-06 10:28:27 +0700

    Front-end ArticleList Component - version 1 & Home Page - version 6

---
+++ front-end/src/Components/Home/ArticleList/ArticleList.jsx
@@ -0,0 +1,39 @@
+import "./article-list.css";
+
+import image from "../../../assets/art-tag.jpeg";
+
+const ArticleList = ({ articles }) => {
+  const [firstArticle, ...restArticles] = articles;
+
+  return (
+    <>
+      <div className="article-list">
+        <div
+          className="list-big-article"
+          style={{ backgroundImage: `url(${firstArticle.thumbnail})` }}
+        >
+          <div className="list-big-article-info">
+            <div className="list-big-article-title">{firstArticle.title}</div>
+            <div className="list-big-article-para">{firstArticle.content}</div>
+          </div>
+        </div>
+        <div className="list-small-items">
+          {restArticles.map((article, index) => (
+            <div key={index} className="list-small-card">
+              <div
+                className="list-small-card-img"
+                style={{ backgroundImage: `url(${article.thumbnail})` }}
+              ></div>
+              <div className="list-small-card-info">
+                <div className="list-small-card-title">{article.title}</div>
+                <div className="list-small-card-para">{article.content}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ArticleList;
--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import {
   faTags,
   faMedal,
   faHeart,
+  faNewspaper,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -17,13 +18,15 @@ import image from "../../assets/sport-tag.jpeg";
 
 import "./home.css";
 import ArticlePanel from "../../Components/Home/ArticlePanel/ArticlePanel";
+import ArticleList from "../../Components/Home/ArticleList/ArticleList";
 
 const Home = () => {
   const exampleArticle = {
     thumbnail: image,
-    title: "Opening Day of the Boating Season so let's set sail",
+    title:
+      "Opening Day of the Boating Season so let's set sail to the big blue sea",
     content:
-      "The text-overflow property specifies how overflowed content that is not displayed should be signaled to the user. It can display a lot of contnet dmnakwdnj dan",
+      "So, you finally went to your first boxing class and learned the basics of the sport. You also learned that it’s recommended to wrap your hands before putting on the gloves. But there are times when you just don’t feel like wrapping them and you wonder why you even need them. Well, this blog is going to explain the benefits of wrapping your hands.",
     author: {
       name: "James adnaklndnadkjn",
       avatar: "https://picsum.photos/200/300",
@@ -31,7 +34,17 @@ const Home = () => {
     time: "2 hours ago",
   };
 
-  const articleList = [
+  const articleSliderList = [
+    exampleArticle,
+    exampleArticle,
+    exampleArticle,
+    exampleArticle,
+    exampleArticle,
+  ];
+
+  const newArticleList = [
+    exampleArticle,
+    exampleArticle,
     exampleArticle,
     exampleArticle,
     exampleArticle,
@@ -62,7 +75,7 @@ const Home = () => {
           </h2>
         </div>
         <div className="home-section-content">
-          <ArticleSlider articles={articleList} />
+          <ArticleSlider articles={articleSliderList} />
         </div>
       </div>
       <div className="home-section">
@@ -95,6 +108,19 @@ const Home = () => {
             <ArticleCard article={exampleArticle} />
             <ArticleCard article={exampleArticle} />
           </div>
+        </div>
+      </div>
+      <div className="home-section">
+        <div className="home-section-banner">
+          <h2>
+            <FontAwesomeIcon icon={faNewspaper} /> New
+          </h2>
+          <a href="" className="show-all-btn">
+            Show all <FontAwesomeIcon icon={faChevronRight} />
+          </a>
+        </div>
+        <div className="home-section-content">
+          <ArticleList articles={newArticleList} />
         </div>
       </div>
       <Footer />
