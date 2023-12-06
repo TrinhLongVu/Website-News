5f22cd9884fd9e87b99d90e5703b915d3c143ee7
caokhanh2k3 <127649054+caokhanh2k3@users.noreply.github.com>
2023-12-06 23:23:11 +0700

    back-end complete createAllUser, FollowWriter, UnfollowWriter version 3

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -67,6 +67,47 @@ exports.createUser = async (req, res, next) => {
     }
 }
 
+exports.createAllUser = async (req, res, next) => {
+    try {
+
+        const filePath = `${__dirname}data\\user.json`.replace('controllers', '');
+        const users = JSON.parse(fs.readFileSync(filePath, 'utf-8')).user;
+
+        //=================================================================================
+        for (const user of users) {
+            const { UserName } = user;
+
+            try {
+                // Check if username is taken
+                const isTaken = await User.findOne({ UserName });
+
+                // If username is taken, log a message and continue to the next iteration
+                if (isTaken) {
+                    console.log(`Username '${UserName}' is already taken. Skipping user creation.`);
+                    continue;
+                }
+
+                // If username is not taken, create the user
+                await User.create(user);
+
+            } catch (error) {
+                // Handle any other errors that might occur during user creation
+                console.error(`Error creating user with username '${UserName}':`, error);
+            }
+        }
+
+        res.status(201).json({
+            status: 'success'
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
+
+}
+
 
 exports.getUser = async (req, res, next) => {
     try {
@@ -104,7 +145,7 @@ exports.updateUser = async (req, res, next) => {
 
         const update = await User.findByIdAndUpdate(_id, req.body, {
             new: true
-        })
+        });
 
         if (!update) {
             return res.status(404).json({
@@ -133,6 +174,7 @@ exports.deleteUser = async (req, res, next) => {
 
         // Find the user by ID and delete it
         const deletedUser = await User.deleteOne({ _id });
+        // const deletedUser = await User.deleteMany();
 
         if (!deletedUser) {
             // If the user with the specified ID is not found, return an error response
@@ -152,4 +194,174 @@ exports.deleteUser = async (req, res, next) => {
             msg: err
         })
     }
+}
+
+
+
+exports.followWriter = async (req, res, next) => {
+    try {
+
+        const _id_writer = req.params.id;
+
+        //====================find writer====================
+        // Find the user by ID 
+        const find_user_writer = await User.findById(_id_writer);
+
+        if (!find_user_writer) {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Writer not found.',
+            });
+        }
+
+        //==== Check writer, If role is "writer"
+
+        if (find_user_writer.Role != "writer") {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Not Writer.',
+            });
+        }
+
+
+        //====================find user====================
+        const _id_user = req.body._id;
+        // Find the user by ID 
+        const find_user_user = await User.findById(_id_user);
+
+        if (!find_user_user) {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Writer not found.',
+            });
+        }
+
+
+        //========================== PUSH ID =========================================
+
+        find_user_user.ID_follow_writer.push(_id_writer);
+        find_user_writer.ID_user_follow.push(_id_user);
+
+        //========================== UPDATE WRITE AND USER=========================================
+
+        const update_writer = await User.findByIdAndUpdate(_id_writer, find_user_writer, {
+            new: true
+        })
+
+
+        const update_user = await User.findByIdAndUpdate(_id_user, find_user_user, {
+            new: true
+        })
+
+        //=========================================================================
+
+
+        if (!update_writer || !update_user) {
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Follow fail.',
+            });
+        }
+
+        res.status(201).json({
+            status: 'success',
+            data: { update_user, update_writer }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
+    next();
+}
+
+
+exports.UnfollowWriter = async (req, res, next) => {
+    try {
+
+        const _id_writer = req.params.id;
+
+        //====================find writer====================
+        // Find the user by ID 
+        const find_user_writer = await User.findById(_id_writer);
+
+        if (!find_user_writer) {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Writer not found.',
+            });
+        }
+
+        //==== Check writer, If role is "writer"
+
+        if (find_user_writer.Role != "writer") {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Not Writer.',
+            });
+        }
+
+
+        //====================find user====================
+        const _id_user = req.body._id;
+        // Find the user by ID 
+        const find_user_user = await User.findById(_id_user);
+
+        if (!find_user_user) {
+            // If the user with the specified ID is not found, return an error response
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Writer not found.',
+            });
+        }
+
+
+        //========================== Delete ID =========================================
+
+        find_user_user.ID_follow_writer = find_user_user.ID_follow_writer.filter(function (element) {
+            return element !== _id_writer;
+        });
+
+        find_user_writer.ID_user_follow = find_user_writer.ID_user_follow.filter(function (element) {
+            return element !== _id_user;
+        });
+
+        //========================== UPDATE WRITE AND USER=========================================
+
+        const update_writer = await User.findByIdAndUpdate(_id_writer, find_user_writer, {
+            new: true
+        })
+
+
+        const update_user = await User.findByIdAndUpdate(_id_user, find_user_user, {
+            new: true
+        })
+
+        //=========================================================================
+
+
+        if (!update_writer || !update_user) {
+            return res.status(404).json({
+                status: 'fail',
+                msg: 'Unfollow fail.',
+            });
+        }
+
+        res.status(201).json({
+            status: 'success',
+            data: { update_user, update_writer }
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            msg: err
+        })
+    }
+    next();
 }
--- back-end/routes/userRouters.js
+++ back-end/routes/userRouters.js
@@ -10,9 +10,23 @@ router
     .post(userController.createUser);
 
 router
+    .route('/create/createAll')
+    .get(userController.createAllUser)
+
+router
     .route('/:id')
     .get(userController.getUser)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
+
+
+
+router
+    .route('/follow/:id')  // :id of writer
+    .patch(userController.followWriter)
+
+router
+    .route('/unfollow/:id')  // :id of writer
+    .patch(userController.UnfollowWriter)
 
 module.exports = router;
